4fc965e1d508b67d711c55731b7976bbf1acd9d5
Ed Cornejo <zed.hyde@icloud.com>
2020-08-16 23:31:32 -0500

    add single responsability exercise

---
+++ solid/single-responsability-exercise/src/main/java/com/dev/core/Employee.java
@@ -0,0 +1,19 @@
+package com.dev.core;
+
+public class Employee {
+	private final String name;
+	private final String position;
+
+	public Employee(String name, String position) {
+		this.name = name;
+		this.position = position;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String printTimeSheetReport() {
+		return this.name + " " + this.position;
+	}	
+}
